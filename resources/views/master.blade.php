7c12119aaed3c809bfc482aa1c92a2de4afa37a2
sdcalz1811 <samuelcas@gmail.com>
2020-03-12 14:49:43 +0800

    First commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <script src="/js/jquery3.2.1.js"></script>
+
+    
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
